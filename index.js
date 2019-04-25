abc4700db871c99a3b285fa6d3169c76d36dd21c
RajiBabu <31910497+RajiBabu@users.noreply.github.com>
2019-04-25 00:15:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express=require('express')
 var app = express()
 
 app.get('/', function(req, res){
-    res.send('Born to conquer!')
+    res.send('Build completed!')
 })
 
 app.listen(3000, function(){
